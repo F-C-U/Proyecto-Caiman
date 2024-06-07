3551020983edd82e2ffa0673281183a105fa07bb
CETECOM <cetecom_conce@duoc.cl>
2024-06-07 17:05:04 -0400

    forms agregarproducto.html listo (falta la base)

--- caiman/forms.py
+++ caiman/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import *
 
-class ProductoForm(forms.Form):
+class ProductoForm(forms.ModelForm):
     class Meta:
         model=Producto
         fields=['nombre','descripcion','cantidad','precio','imagen']
--- caiman/views.py
+++ caiman/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
-from .models import Producto
 from .forms import ProductoForm
 
 # Create your views here.
 def agregarproducto(request):
     form=ProductoForm()
-    print(form)
     if request.method=="POST":
         form=ProductoForm(data=request.POST,files=request.FILES)
         if form.is_valid():
