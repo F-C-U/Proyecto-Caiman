6b009fbc6ddbcb5e6115617d5690e600bf14cd3c
CETECOM <cetecom_conce@duoc.cl>
2024-06-21 16:55:50 -0400

    closes  #15, closes #14, closes #13; Crud listo

--- caiman/urls.py
+++ caiman/urls.py
@@ -1,7 +1,14 @@
 from django.urls import include, path
-from .views import agregarproducto, modificarproducto
+from .views import productos, agregarproducto, modificarproducto, eliminarproducto
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path('productos/', productos, name='productos'),
     path('agregarproducto/', agregarproducto, name='agregarproducto'),
     path('modificarproducto/<id>', modificarproducto, name='modificarproducto'),
+    path('eliminarproducto/<id>', eliminarproducto, name='eliminarproducto'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- caiman/views.py
+++ caiman/views.py
@@ -3,14 +3,26 @@ from .forms import ProductoForm, UpdateProductoForm
 from django.shortcuts import get_object_or_404, redirect
 from django.contrib import messages
 from .models import Producto
+from os import remove, path
+from django.conf import settings
 
 # Create your views here.
+def productos(request):
+    productos=Producto.objects.all()
+    datos={
+        "productos":productos
+    }
+
+
+    return render(request,'caiman/productos.html',datos)
+
 def agregarproducto(request):
     form=ProductoForm()
     if request.method=="POST":
         form=ProductoForm(data=request.POST,files=request.FILES)
         if form.is_valid():
             form.save()
+            return redirect(to="productos")
 
     datos={
         "form":form
@@ -29,6 +41,7 @@ def modificarproducto(request, id):
         form=UpdateProductoForm(data=request.POST,files=request.FILES,instance=producto)
         if form.is_valid():
             form.save()
+        return redirect(to="productos")
     
     datos={
         "form":form,
@@ -36,3 +49,17 @@ def modificarproducto(request, id):
     }
 
     return render(request,'caiman/modificarproducto.html',datos)
+
+def eliminarproducto(request, id):
+    producto=get_object_or_404(Producto, id=id)
+    
+    if request.method=="POST":
+        remove(path.join(str(settings.MEDIA_ROOT).replace('/media',''))+producto.imagen.url)
+        producto.delete()
+        return redirect(to="productos")
+        
+    datos={
+        "producto":producto
+    }
+
+    return render(request,'caiman/eliminarproducto.html',datos)
